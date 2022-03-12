6f4a02fec7f09d09a49004ab609ac674a91b2f42
damiankain <damiankain666@gmail.com>
2022-03-12 19:41:12 +0300

    Task 1 is done

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -1,7 +1,19 @@
 package Task1;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    }
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+
+BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите свой вес:");
+
+        double myWeight = Double.parseDouble(reader.readLine());
+        double weightOnMoon = myWeight * 0.16;
+
+        System.out.println("Ваш вес на Луне:");
+        System.out.println(weightOnMoon);
+        }
 }
