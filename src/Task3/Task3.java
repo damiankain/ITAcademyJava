e9823cfb2feaed3ca5736ab362a94677348eefca
damiankain <damiankain666@gmail.com>
2022-03-13 11:51:06 +0300

    Task 3 is done

--- src/Task3/Task3.java
+++ src/Task3/Task3.java
@@ -1,2 +1,25 @@
-package Task3;public class Task3 {
+package Task3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task3 {
+    public static void main(String[] args) throws IOException {
+       checkPalindrom();
+
+    }
+
+    public static void checkPalindrom() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstLine = reader.readLine();
+        String secondLine = new StringBuilder(firstLine).reverse().toString();
+
+        if (secondLine.equals(firstLine)) {
+            System.out.println("Это палиндром");
+        } else {
+            System.out.println("Это не палиндром");
+        }
+
+    }
 }
