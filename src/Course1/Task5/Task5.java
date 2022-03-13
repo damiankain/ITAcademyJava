0cdbe47fdf6c30fb13c249e2daebd15ad06ed1cb
damiankain <damiankain666@gmail.com>
2022-03-13 12:40:21 +0300

    Task 4 is done

---
+++ src/Course1/Task5/Task5.java
@@ -0,0 +1,10 @@
+package Course1.Task5;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+            i++;
+            }
+    }
+}
